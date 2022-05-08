87c95091f25af1dd06588a30a860abd0686fddf8
Jeevan Mallik <jeevanmalli47@gmail.com>
2022-05-09 00:47:12 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer class="footer"  style="background-color: black";>
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3 style="color: #fff";>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <!-- <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="wishlist.php"> <i class="fas fa-angle-right"></i> wishlist</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> register</a>
+      </div> -->
+
+      <div class="box">
+         <h3 style="color: #fff";>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +987-654-3210 </p>
+         <p> <i class="fas fa-envelope"></i> dailyneeds@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Bengaluru,Karnataka,India - 560064 </p>
+      </div>
+
+      <div class="box">
+         <h3 style="color: #fff";>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </section>
+
+   <h1 style="padding: 20px; text-align: center; color: #fff"> &copy; copyright 2022 by DailyNeeds All Rights Reserved! </h1>
+
+</footer>
